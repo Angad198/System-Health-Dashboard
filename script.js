4356c416f818f0672b76c0a74a1e6d5b77a6362c
Angad198 <angad4082@gmail.com>
2026-06-02 16:55:13 +1200

    Added premium login system and dashboard improvements

--- script.js
+++ script.js
@@ -82,6 +82,32 @@ function updateDashboard(){
     }
 }
 
+
 setInterval(updateDashboard, 2000);
 
 updateDashboard();
+
+function login(){
+
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    if(
+        username === "admin"
+        &&
+        password === "admin123"
+    ){
+        window.location.href =
+        "index.html";
+    }
+
+    else{
+        document.getElementById(
+            "error-message"
+        ).innerText =
+        "Invalid Username or Password";
+    }
+}
