890714b2483cf70b2bdadd6087b6cb8682457ddf
Angad198 <angad4082@gmail.com>
2026-06-02 16:48:00 +1200

    Initial commit - System Health Dashboard

---
+++ script.js
@@ -0,0 +1,87 @@
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const logs = [
+    "Windows Defender Scan Completed",
+    "CPU Spike Detected",
+    "RAM Optimized Successfully",
+    "Security Patch Installed",
+    "Network Stable",
+    "Firewall Active",
+    "Disk Cleanup Recommended",
+    "Background Service Restarted",
+    "System Running Smoothly",
+    "No Threats Detected"
+];
+
+function updateDashboard(){
+
+    let cpu = randomNumber(20,95);
+    let ram = randomNumber(30,90);
+    let disk = randomNumber(25,85);
+
+    document.getElementById("cpu")
+        .innerText = cpu + "%";
+
+    document.getElementById("ram")
+        .innerText = ram + "%";
+
+    document.getElementById("disk")
+        .innerText = disk + "%";
+
+    // Progress bars
+    document.getElementById("cpu-bar")
+        .style.width = cpu + "%";
+
+    document.getElementById("ram-bar")
+        .style.width = ram + "%";
+
+    document.getElementById("disk-bar")
+        .style.width = disk + "%";
+
+    // Alerts
+    let alertMessage =
+        "✅ System Running Smoothly";
+
+    if(cpu > 80){
+        alertMessage =
+        "⚠️ High CPU Usage Detected";
+    }
+
+    if(ram > 80){
+        alertMessage =
+        "⚠️ RAM Usage Critical";
+    }
+
+    document.getElementById("alert-message")
+        .innerText = alertMessage;
+
+    // Logs
+    let randomLog =
+        logs[Math.floor(Math.random() * logs.length)];
+
+    let logContainer =
+        document.getElementById("logs");
+
+    let newLog =
+        document.createElement("div");
+
+    newLog.classList.add("log");
+
+    newLog.innerText =
+        "[" + new Date().toLocaleTimeString() + "] "
+        + randomLog;
+
+    logContainer.prepend(newLog);
+
+    if(logContainer.children.length > 8){
+        logContainer.removeChild(
+            logContainer.lastChild
+        );
+    }
+}
+
+setInterval(updateDashboard, 2000);
+
+updateDashboard();
